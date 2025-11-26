d973fec5db047e3052c7c02e9c1c9c6c0dd6c8a0
Andrey K <andrey.kozhevnikov@devexpress.com>
2025-11-26 12:23:57 +0400

    remove objects

--- CS/EF/NavigationItemEF/NavigationItemEF.Module/Module.cs
+++ CS/EF/NavigationItemEF/NavigationItemEF.Module/Module.cs
@@ -19,7 +19,6 @@ public sealed class NavigationItemEFModule : ModuleBase {
 		// NavigationItemEFModule
 		// 
 		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
-		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule));
     }
     public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
         ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
